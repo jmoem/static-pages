2f1bc76b91b70f972787c6dcadd0693b01a602d0
John Mc Donald <20039253@mail.wit.ie>
2012-10-07 14:41:16 +0100

    Finished Static Pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytwitter::Application.config.secret_token = 'a759ef7b9fb67b1172a21a0ae38d1b6ea007c5212343406d220af7a5a7a3dbb68dcbd11d4b920dd0f7b0440af38262df07b3fb34c13a30d88248d972006c4d51'
